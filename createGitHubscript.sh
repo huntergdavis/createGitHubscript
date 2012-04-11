1426a3d71658bcc80b4adba8832a6ff2f6456e87
huntergdavis <hunter@hunterdavis.com>
2012-04-11 14:00:19 -0700

    updated script to read in options from cli

--- createGitHubscript.sh
+++ createGitHubscript.sh
@@ -3,3 +3,15 @@
 # a shell script to iterate over directories
 # and create github repositories based on the information given.
 
+# we want k for api key, x for prefix, u for username, h for help
+
+while getopts "k:x:h:u:" opt;
+do
+case $opt in
+k) API_KEY=$OPTARG;;
+x) PREFIX=$OPTARG ;;
+u) USERNAME=$OPTARG ;;
+h) echo "Usage: createGitHubscript.sh -k GITHUB_API_KEY -u GITHUB_USERNAME -x DESCRIPTION_PREFIX" ; exit 1 ;;
+*) echo "Usage: createGitHubscript.sh -k GITHUB_API_KEY -u GITHUB_USERNAME -x DESCRIPTION_PREFIX" ; exit 1 ;;
+esac
+done
